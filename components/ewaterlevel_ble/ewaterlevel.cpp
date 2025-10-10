877566331a859618fdaddc888c673c483957089c
Evgeny Ivashchenko <Evgeny.ivashchenko@gmail.com>
2025-10-10 12:15:29 +0200

    refactor BLE scan result payload length retrieval

--- components/ewaterlevel_ble/ewaterlevel.cpp
+++ components/ewaterlevel_ble/ewaterlevel.cpp
@@ -47,7 +47,7 @@ bool EWaterLevel::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
 
   const esphome::esp32_ble::BLEScanResult &scan_result = device.get_scan_result();
   const uint8_t *payload = scan_result.get_adv_data();
-  uint8_t len = scan_result.get_payload_length();
+  uint8_t len = scan_result.get_adv_data_len();
   if (len == sizeof(ewaterlevel_data)) {
     const ewaterlevel_data *data = (ewaterlevel_data *) payload;
     if (!data->validate_header()) {
