ea52a9c933b2e05951e8cc241f98fed06e4611e7
Evgeny Ivashchenko <Evgeny.ivashchenko@gmail.com>
2025-10-15 11:18:15 +0200

    refactor: comment out read_counter function and related logging in ewaterlevel

--- components/ewaterlevel_ble/ewaterlevel.h
+++ components/ewaterlevel_ble/ewaterlevel.h
@@ -62,7 +62,7 @@ struct ewaterlevel_data {  // NOLINT(readability-identifier-naming,altera-struct
    */
   u_int8_t state_c;
   u_int8_t empty;
-  u_int8_t empty;
+  u_int8_t empty2;
 
   // inline bool validate_header() const {
   //   return this->preamble[0] == 0x02 && this->preamble[1] == 0x01 && this->preamble[2] == 0x06 &&
